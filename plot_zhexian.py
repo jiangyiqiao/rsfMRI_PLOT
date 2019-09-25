ef2c610168ff15ff5dfa1b78c070364bc60e4cde
jiangyiqiao <yiqiaoer@gmail.com>
2019-09-25 11:12:00 +0800

    ‘。’

--- plot_zhexian.py
+++ plot_zhexian.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 
 x = [i/10 for i in range(9)]
 x_label = [i/10 for i in range(10)]
-#
+
+# NC vs. EMCI
 # y_1 = [86.82,86.82,85.63,86.22,86.82,86.22,86.22,86.22,86.22]
 # y_2 = [88.02,88.62,88.02,88.02,88.62,88.62,89.82,88.62,88.62]
 # y_3 = [89.22,89.22,89.22,89.82,89.22,89.82,90.42,89.82,90.42]
@@ -19,19 +20,22 @@ y_1 = [86.13,87.3,86.13,86.13,87.3,85.29,84.45,84.45,84]
 y_2 = [87.39,87.39,87.39,86.13,86.13,87.5,86.,86.13,87.39]
 y_3 = [87.39,87.82,86.55,86.55,89.5,87.39,85.29,87.39,86.13]
 
-plt.plot(x, y_1, marker='+', color='black',ms=5, mec='r', mfc='w', label='proposed SA-BiGRU')
-plt.plot(x, y_2, marker='*', ms=5, label='BiLSTM')
-plt.plot(x, y_3, marker='o', color='red',ms=6, label='BiGRU')
-plt.legend()  # 让图例生效
+plt.plot(x, y_1, marker='+', color='black',ms=5, mec='r', mfc='w', label='BiLSTM')
+plt.plot(x, y_2, marker='*', ms=5, label='BiGRU')
+plt.plot(x, y_3, marker='o', color='red',ms=6, label='proposed SA-BiGRU')
+plt.legend(loc='lower left')  # 让图例生效
 
-plt.xlabel('Obeserved ROI noise covariance (R1)')  # X轴标签
+plt.xlabel('Variance of Adaptive Correlation Coefficient (R1)')  # X轴标签
 plt.ylabel("ACC(%)")  # Y轴标签
-pyplot.yticks([75, 80, 85, 90, 95])
+pyplot.yticks([80, 85, 90, 95])
+
 # plt.title("NC vs. EMCI") #标题
-# plt.title("EMCI vs. LMCI") #标题
-plt.title("NC vs. EMCI vs. LMCI") #标题
 # plt.savefig('images/zhexian_acc/nc_emci.png', dpi=900)
+
+# plt.title("EMCI vs. LMCI") #标题
 # plt.savefig('images/zhexian_acc/emci_lmci.png', dpi=900)
+
+plt.title("NC vs. EMCI vs. LMCI") #标题
 plt.savefig('images/zhexian_acc/nc_emci_lmci.png', dpi=900)
 plt.show()
 
